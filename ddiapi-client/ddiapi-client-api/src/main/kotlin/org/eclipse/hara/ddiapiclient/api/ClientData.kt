24dc39167724006bfa7f3d5f8d34135659f75433
Aleksandrov Aleksander <aaleksandrov@dev.rtsoft.ru>
2022-05-18 18:14:28 +0300

    Minify to ddi-api only. Change okhttp dep version
    code call fixes
    
    fix versions for maven enforcer.DependencyConvergence

--- ddiapi-client/ddiapi-client-api/src/main/kotlin/org/eclipse/hara/ddiapiclient/api/ClientData.kt
+++ ddiapi-client/ddiapi-client-api/src/main/kotlin/org/eclipse/hara/ddiapiclient/api/ClientData.kt
@@ -1,31 +1,9 @@
-/*
-* Copyright © 2017-2021  Kynetics  LLC
-*
-* This program and the accompanying materials are made
-* available under the terms of the Eclipse Public License 2.0
-* which is available at https://www.eclipse.org/legal/epl-2.0/
-*
-* SPDX-License-Identifier: EPL-2.0
-*/
-
-package org.eclipse.hara.ddiclient.core.api
+package org.eclipse.hara.ddiapiclient.api
 
 import java.net.MalformedURLException
 import java.net.URL
 
-/**
- * Configuration data of Hara client.
- * @property tenant name of the tenant (targets group)
- * on the Update Server to connect to.
- * @property controllerId id used by the client to register on
- * the Update Server.
- * @property serverUrl URL of the Update Server.
- * @property gatewayToken used by the client to authenticate itself
- * on the Update Server. It is different for each tenant.
- * @property targetToken used by the client to authenticate itself
- * on the Update Server. It is different for each device.
- */
-data class HaraClientData constructor(
+data class ClientData constructor(
         val tenant: String,
         val controllerId: String,
         val serverUrl: String,
--- ddiapi-client/src/main/kotlin/org/eclipse/hara/ddiapiclient/api/DdiClientDefaultImpl.kt
+++ ddiapi-client/src/main/kotlin/org/eclipse/hara/ddiapiclient/api/DdiClientDefaultImpl.kt
@@ -24,8 +24,6 @@ import java.net.HttpURLConnection
 import java.util.HashSet
 import java.util.concurrent.Executors
 import okhttp3.OkHttpClient
-import org.eclipse.hara.ddiclient.core.api.HaraClientData
-import org.slf4j.LoggerFactory
 import retrofit2.HttpException
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -34,14 +32,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 class DdiClientDefaultImpl private constructor(private val ddiRestApi: DdiRestApi, private val tenant: String, private val controllerId: String) : DdiClient {
 
     override suspend fun getSoftwareModulesArtifacts(softwareModuleId: String): List<ArtifactResponse> {
-        LOG.debug("getSoftwareModulesArtifacts({})", softwareModuleId)
         val artifact = ddiRestApi.getSoftwareModulesArtifacts(tenant, controllerId, softwareModuleId)
-        LOG.debug("{}", artifact)
         return artifact
     }
 
     override suspend fun putConfigData(data: ConfigurationDataRequest, onSuccessConfigData: () -> Unit) {
-        LOG.debug("putConfigData({})", ConfigurationDataRequest)
         val responseCode = ddiRestApi.putConfigData(tenant, controllerId, data).code()
         if (responseCode in 200 until 300) {
             onSuccessConfigData.invoke()
@@ -49,52 +44,39 @@ class DdiClientDefaultImpl private constructor(private val ddiRestApi: DdiRestAp
     }
 
     override suspend fun getControllerActions(): ControllerBaseResponse {
-        LOG.debug("getControllerActions()")
         val response = ddiRestApi.getControllerActions(tenant, controllerId)
-        LOG.debug("{}", response)
         return handleResponse(response)
     }
 
     override suspend fun onControllerActionsChange(etag: String, onChange: OnResourceChange<ControllerBaseResponse>) {
-        LOG.debug("onDeploymentActionDetailsChange({})", etag)
         val response = ddiRestApi.getControllerActions(tenant, controllerId, etag)
-        LOG.debug("{}", response)
         handleOnChangeResponse(response, etag, "BaseResource", onChange)
     }
 
     override suspend fun getDeploymentActionDetails(actionId: String, historyCount: Int): DeploymentBaseResponse {
-        LOG.debug("getDeploymentActionDetails($actionId, $historyCount)")
         val response = ddiRestApi.getDeploymentActionDetails(tenant, controllerId, actionId, null, historyCount)
-        LOG.debug("{}", response)
         return handleResponse(response)
     }
 
     override suspend fun onDeploymentActionDetailsChange(actionId: String, historyCount: Int, etag: String, onChange: OnResourceChange<DeploymentBaseResponse>) {
-        LOG.debug("onDeploymentActionDetailsChange($actionId, $historyCount, $etag)")
         val response = ddiRestApi.getDeploymentActionDetails(tenant, controllerId, actionId, null, historyCount, etag)
-        LOG.debug("{}", response)
         handleOnChangeResponse(response, etag, "Deployment", onChange)
     }
 
     override suspend fun getCancelActionDetails(actionId: String): CancelActionResponse {
-        LOG.debug("getCancelActionDetails($actionId)")
         val response = ddiRestApi.getCancelActionDetails(tenant, controllerId, actionId)
-        LOG.debug("{}", response)
         return response
     }
 
     override suspend fun postDeploymentActionFeedback(actionId: String, feedback: DeploymentFeedbackRequest) {
-        LOG.debug("postDeploymentActionFeedback({},{})", actionId, feedback)
         ddiRestApi.postDeploymentActionFeedback(tenant, controllerId, actionId, feedback)
     }
 
     override suspend fun postCancelActionFeedback(actionId: String, feedback: CancelFeedbackRequest) {
-        LOG.debug("postCancelActionFeedback({},{})", actionId, feedback)
         ddiRestApi.postCancelActionFeedback(tenant, controllerId, actionId, feedback)
     }
 
     override suspend fun downloadArtifact(url: String): InputStream {
-        LOG.debug("downloadArtifact({})", url)
         return ddiRestApi.downloadArtifact(url).byteStream()
     }
 
@@ -102,12 +84,10 @@ class DdiClientDefaultImpl private constructor(private val ddiRestApi: DdiRestAp
         when (response.code()) {
             in 200..299 -> {
                 val newEtag = response.headers()[ETAG_HEADER] ?: ""
-                LOG.info("{} is changed. Old ETag: {}, new ETag: {}", resourceName, etag, newEtag)
                 onChange.invoke(response.body()!!, newEtag)
             }
 
-            HttpURLConnection.HTTP_NOT_MODIFIED -> LOG.info("{} not changed", resourceName)
-
+            HttpURLConnection.HTTP_NOT_MODIFIED -> {}
             else -> throw HttpException(response)
         }
     }
@@ -122,9 +102,7 @@ class DdiClientDefaultImpl private constructor(private val ddiRestApi: DdiRestAp
     companion object {
         const val ETAG_HEADER = "ETag"
 
-        val LOG = LoggerFactory.getLogger(DdiClient::class.java)!!
-
-        fun of(haraClientData: HaraClientData, httpBuilder:OkHttpClient.Builder): DdiClientDefaultImpl {
+        fun of(haraClientData: ClientData, httpBuilder:OkHttpClient.Builder): DdiClientDefaultImpl {
             val authentications = HashSet<Authentication>()
             with(haraClientData) {
                 if (gatewayToken != null) {
